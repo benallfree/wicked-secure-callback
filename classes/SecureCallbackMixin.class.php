43f7551cba25f26b4279e46b57b24821b4c0a334
Ben Allfree <ben@benallfree.com>
2012-08-17 07:45:52 -0700

    Refactor to support new event structure and mixins

--- classes/SecureCallbackMixin.class.php
+++ classes/SecureCallbackMixin.class.php
@@ -4,6 +4,35 @@ class SecureCallbackMixin extends Mixin
 {
   static $__prefix = "sc";
   
+  static function init()
+  {
+    parent::init();
+
+    W::register_action('secure_callback_before_validate', function($sc) {
+      if (!$sc->token)
+      {
+        $sc->token = md5(json_encode($sc->data) . uniqid() . microtime(true));
+      }
+      return $sc;
+    });
+    
+    W::register_action('secure_callback_serialize', function($sc) {
+      $sc->data = json_encode($sc->data);
+      return $sc;
+    });
+    
+    W::register_action('secure_callback_unserialize', function($sc) {
+      $sc->data = json_decode($sc->data,true);
+      return $sc;
+    });
+    
+    SecureCallback::add_function('expire', function($sc) {
+      if(!$sc->expires_at) return;
+      $sc->used_at = time();
+      $sc->save();
+    });    
+  }
+  
   static function create_url($func_name)
   {
     $args = func_get_args();
--- load.php
+++ load.php
@@ -1,1 +1,3 @@
 <?
+
+W::add_mixin('SecureCallbackMixin');
